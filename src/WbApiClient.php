e86bd156e08872f42a93b432c4001da5ac0a1ae0
Пархомей Алексей <parhomey@eurosvet.com>
2025-12-18 17:27:29 +0300

    add service provider and main client class

---
+++ src/WbApiClient.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Parhomeiai\WbApiClient;
+
+class WbApiClient
+{
+    protected string $apiKey;
+
+    public function __construct(?string $apiKey)
+    {
+        $this->apiKey = $apiKey ?? '';
+    }
+
+    public function ping(): string
+    {
+        return 'WB API client works';
+    }
+}
---
+++ src/WbApiClientServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Parhomeiai\WbApiClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class WbApiClientServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/wb-api-client.php',
+            'wb-api-client'
+        );
+
+        $this->app->singleton(WbApiClient::class, function () {
+            return new WbApiClient(
+                config('wb-api-client.api_key')
+            );
+        });
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/wb-api-client.php' =>
+                config_path('wb-api-client.php'),
+        ], 'wb-api-client-config');
+    }
+}
