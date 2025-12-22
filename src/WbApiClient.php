6f69631459b36bdcf11d76326bee95490091801c
Пархомей Алексей <parhomey@eurosvet.com>
2025-12-22 13:54:17 +0300

    fix namespace

--- src/WbApiClient.php
+++ src/WbApiClient.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Parhomeiai\WbApiClient;
+namespace Escorp\WbApiClient;
 
 class WbApiClient
 {
--- src/WbApiClientServiceProvider.php
+++ src/WbApiClientServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Parhomeiai\WbApiClient;
+namespace Escorp\WbApiClient;
 
 use Illuminate\Support\ServiceProvider;
 
