1b648207559fdf601b3c04c430a7ce3c3ebc59ec
Пархомей Алексей <parhomey@eurosvet.com>
2025-12-23 13:26:01 +0300

    Add exception

---
+++ src/Exceptions/WbApiClientException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Escorp\WbApiClient\Exceptions;
+
+use RuntimeException;
+use Throwable;
+
+final class WbApiClientException extends RuntimeException
+{
+    public static function fromException(Throwable $e): self
+    {
+        return new self($e->getMessage(), (int) $e->getCode(), $e);
+    }
+}
+
