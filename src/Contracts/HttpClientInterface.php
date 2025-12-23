8a70df70cdbb430b46926e8511d2ae14162bd28b
Пархомей Алексей <parhomey@eurosvet.com>
2025-12-23 13:02:53 +0300

    modify http contracts

--- src/Contracts/HttpClientInterface.php
+++ src/Contracts/HttpClientInterface.php
@@ -2,7 +2,10 @@
 
 namespace Escorp\WbApiClient\Contracts;
 
+use Psr\Http\Message\ResponseInterface;
+
 interface HttpClientInterface
 {
     public function request(string $method, string $url, array $options = []): array;
+    public function requestRaw(string $method, string $url, array $options = []): ResponseInterface;
 }
